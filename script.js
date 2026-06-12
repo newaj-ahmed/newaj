c1ab28d7bf99849fdaec27e1f69fd9a9305737b8
newaj-ahmed <newaj4951@gmail.com>
2026-06-12 19:55:30 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -705,6 +705,59 @@ img.style.transition =
 });
 
 });
+const launchDate =
+new Date("July 1, 2026 09:00:00").getTime();
+
+const launchScreen =
+document.getElementById("launch-screen");
+
+const countdownTimer =
+setInterval(function(){
+
+const now =
+new Date().getTime();
+
+const distance =
+launchDate - now;
+
+const days =
+Math.floor(distance / (1000 * 60 * 60 * 24));
+
+const hours =
+Math.floor(
+(distance % (1000 * 60 * 60 * 24))
+/
+(1000 * 60 * 60)
+);
+
+const minutes =
+Math.floor(
+(distance % (1000 * 60 * 60))
+/
+(1000 * 60)
+);
+
+const seconds =
+Math.floor(
+(distance % (1000 * 60))
+/
+1000
+);
+
+document.getElementById("days").innerHTML = days;
+document.getElementById("hours").innerHTML = hours;
+document.getElementById("minutes").innerHTML = minutes;
+document.getElementById("seconds").innerHTML = seconds;
+
+if(distance <= 0){
+
+clearInterval(countdownTimer);
+
+launchScreen.style.display = "none";
+
+}
+
+},1000);
 
 /* =====================================================
 END
